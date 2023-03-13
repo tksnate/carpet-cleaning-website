698342d51a1312a74c526b7776c2ff5a620d15fe
tksnate <tksnate@gmail.com>
2023-03-12 18:45:34 -0700

    align home text box

--- src/components/TextBoxThree.js
+++ src/components/TextBoxThree.js
@@ -18,41 +18,43 @@ import reviewPic2 from "../app/assets/img/gog500.png";
 const TextBoxThree = () => {
   return (
     <Row className="homeTextTwo">
-      {/* <div className="d-flex"> */}
-      <Col sm="3">
-        <img
-          src={reviewPic}
-          alt="5-Star Customer Review of Cleaning Company"
-          style={{ width: "200px" }}
-        />
-      </Col>
-      <Col sm="9">
-        <Card className="mr-5">
-          <CardBody className="mr-3">
-            {" "}
-            <CardText>
-              "We use{" "}
-              <span className="text-primary bold">
-                Valley's Best Carpet & Tile Cleaning
-              </span>{" "}
-              yesterday. The tech arrived on time and was knowledgeable. The
-              quote I got on the phone was the price I paid. No hidden fees!!
-              <span className="bold">
-                {" "}
-                Carpet and tile look amazing. Would definitely recommend.
-              </span>{" "}
-              We wil be using them again!!"
-            </CardText>
-            <Button color="primary" href="contact">
-              Contact Us Today
-            </Button>
-          </CardBody>
-          <CardFooter className="bold">
-            Customer Testimonial - 5-Star Google Review
-          </CardFooter>
-        </Card>
-      </Col>
-      {/* </div> */}
+      <div class="row">
+        <Col sm="4" className="ms-auto">
+          <img
+            src={reviewPic}
+            alt="5-Star Customer Review of Cleaning Company"
+            style={{ width: "200px", display: "block", margin: "auto" }}
+          />
+        </Col>
+        <Col>
+          <div class="customerTestomial">
+            <Card className="ms-5">
+              <CardBody class="customerTest">
+                <CardText>
+                  "We use{" "}
+                  <span className="text-primary bold">
+                    Valley's Best Carpet & Tile Cleaning
+                  </span>{" "}
+                  yesterday. The tech arrived on time and was knowledgeable. The
+                  quote I got on the phone was the price I paid. No hidden
+                  fees!!
+                  <span className="bold">
+                    {" "}
+                    Carpet and tile look amazing. Would definitely recommend.
+                  </span>{" "}
+                  We wil be using them again!!"
+                </CardText>
+                <Button color="primary" href="contact">
+                  Contact Us Today
+                </Button>
+              </CardBody>
+              <CardFooter className="bold">
+                Customer Testimonial - 5-Star Google Review
+              </CardFooter>
+            </Card>
+          </div>
+        </Col>
+      </div>
     </Row>
   );
 };
