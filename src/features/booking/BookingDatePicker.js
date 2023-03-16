5af0697bedb94ba006728dca3add5a063ad6ba5c
tksnate <tksnate@gmail.com>
2023-03-16 11:42:02 -0700

    date picker updte

--- src/features/booking/BookingDatePicker.js
+++ src/features/booking/BookingDatePicker.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { addDays } from "date-fns";
 
 const BookingDatePicker = () => {
   const [selectedDate, setselectedDate] = useState(new Date());
@@ -9,8 +10,8 @@ const BookingDatePicker = () => {
     <DatePicker
       selected={selectedDate}
       onChange={(date) => setselectedDate(date)}
-      //   minDate={addDays(new Date(), 2)}
-      //   maxDate={addDays(new Date(), 30)}
+      minDate={addDays(new Date(), 2)}
+      maxDate={addDays(new Date(), 30)}
       filterDate={(date) => date.getDay() !== 6 && date.getDay() !== 0}
       isClearable
     />
